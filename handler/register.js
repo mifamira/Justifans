83eedf2b776a630002fde44e85810eb80e4f6715
mifamira <150776819+mifamira@users.noreply.github.com>
2024-12-04 20:01:55 +0700

    register.js

--- handler/register.js
+++ handler/register.js
@@ -5,13 +5,7 @@ import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.11
  
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBQ3FFWzz-lBkEajePwUl5LxgpAOGqlXZA",
-    authDomain: "capstone-442413.firebaseapp.com",
-    projectId: "capstone-442413",
-    storageBucket: "capstone-442413.firebasestorage.app",
-    messagingSenderId: "1040333147919",
-    appId: "1:1040333147919:web:63f19a9dd72b315bebb87a",
-    measurementId: "G-S3Q03WCGNW"
+
   };
 
 // Initialize Firebase
